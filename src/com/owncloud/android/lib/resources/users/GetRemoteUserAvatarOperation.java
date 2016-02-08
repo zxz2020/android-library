fad38763bca7300a4944fed4344f14ccffb52b36
Andy Scherzinger <info@andy-scherzinger.de>
2016-02-08 16:57:39 +0100

    wip reading avatar data

---
+++ src/com/owncloud/android/lib/resources/users/GetRemoteUserAvatarOperation.java
@@ -0,0 +1,115 @@
+/* ownCloud Android Library is available under MIT license
+ *   Copyright (C) 2016 ownCloud Inc.
+ *   
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *   
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ *   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+package com.owncloud.android.lib.resources.users;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.operations.RemoteOperation;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+import com.owncloud.android.lib.common.utils.Log_OC;
+
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author AndyScherzinger
+ *
+ * Get the user's avatar image.
+ */
+public class GetRemoteUserAvatarOperation extends RemoteOperation {
+
+	private static final String TAG = GetRemoteUserAvatarOperation.class.getSimpleName();
+
+	// OCS Route
+	private static final String OCS_ROUTE ="/index.php/avatar/";
+
+	public GetRemoteUserAvatarOperation() {
+	}
+
+	@Override
+	protected RemoteOperationResult run(OwnCloudClient client) {
+		// TODO check if isAfter8Version is sufficient
+		if(!client.getOwnCloudVersion().isAfter8Version()) {
+			return new RemoteOperationResult(RemoteOperationResult.ResultCode.BAD_OC_VERSION);
+		}
+
+		RemoteOperationResult result = null;
+		int status = -1;
+		GetMethod get = null;
+
+		//Get the avatar
+		try {
+			String username = client.getCredentials().getUsername().substring(0,
+					client.getCredentials().getUsername().lastIndexOf("@"));
+			// TODO make pixel size configurable via xml config
+			get = new GetMethod(client.getBaseUri() + OCS_ROUTE + username + "/128");
+			get.addRequestHeader(OCS_API_HEADER, OCS_API_HEADER_VALUE);
+			status = client.executeMethod(get);
+
+			if(isSuccess(status)) {
+				 byte[] response = get.getResponseBody();
+				 Bitmap avatar = BitmapFactory.decodeByteArray(response, 0, response.length);
+
+				 Log_OC.d(TAG, "Successful response: " + response.length + " bytes");
+
+				 // Result
+				 result = new RemoteOperationResult(true, status, get.getResponseHeaders());
+				 // avatar in result.data
+                 ArrayList<Object> data = new ArrayList<Object>();
+                 data.add(avatar);
+                 result.setData(data);
+				 
+				 Log_OC.d(TAG, "*** read user avatar information for: " + username);
+				 
+			} else {
+				result = new RemoteOperationResult(false, status, get.getResponseHeaders());
+				String response = get.getResponseBodyAsString();
+				Log_OC.e(TAG, "Failed response while getting user avatar information ");
+				if (response != null) {
+					Log_OC.e(TAG, "*** status code: " + status + " ; response message: " + response);
+				} else {
+					Log_OC.e(TAG, "*** status code: " + status);
+				}
+			}
+		} catch (Exception e) {
+			result = new RemoteOperationResult(e);
+			Log_OC.e(TAG, "Exception while getting OC user information", e);
+			
+		} finally {
+			get.releaseConnection();
+		}
+        
+		return result;
+	}
+
+    private boolean isSuccess(int status) {
+        return (status == HttpStatus.SC_OK);
+    }
+    
+}
