040d81a91be9783cf0d08601c21ab605d192c7f4
davigonz <dgonzalez@solidgear.es>
2017-06-30 13:02:28 +0200

    Rename getAuthernticateHeader method

--- src/com/owncloud/android/lib/common/operations/RemoteOperationResult.java
+++ src/com/owncloud/android/lib/common/operations/RemoteOperationResult.java
@@ -553,7 +553,7 @@ public class RemoteOperationResult implements Serializable {
         return (mRedirectedLocation != null && !(mRedirectedLocation.toLowerCase().startsWith("https://")));
     }
 
-    public ArrayList<String> getAuthenticateHeader() {
+    public ArrayList<String> getAuthenticateHeaders() {
         return mAuthenticate;
     }
 
